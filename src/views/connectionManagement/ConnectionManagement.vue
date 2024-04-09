715c710dedc6a38df4ac69d45892b4e57b4aac12
万叶 <41570069+Jy03684315@users.noreply.github.com>
2024-04-09 15:53:09 +0800

    complete connectionManagement

--- src/views/connectionManagement/ConnectionManagement.vue
+++ src/views/connectionManagement/ConnectionManagement.vue
@@ -8,9 +8,94 @@
       </div>
     </rtc-header>
     <div class="cmContent flex-center">
-      <div v-if="moreRobot">
-        
+      <!-- 正常情况下的机器人列表 -->
+      <div v-if="moreRobot" class="robotBox">
+        <el-carousel
+          indicator-position="none"
+          :autoplay="false"
+          arrow="nerver"
+          height="28.125vw"
+        >
+          <el-carousel-item v-for="(list, i) in carouselList" :key="i">
+            <!-- 添加机器人 -->
+            <div class="carouselItem">
+              <div class="addCard flex-center" v-if="i == 0">
+                <div class="add flex-center">
+                  <img class="addImg" src="@/assets/images/icon_add.png" />
+                </div>
+                <div class="addContent">{{ $t("addRobot") }}</div>
+              </div>
+              <!-- 机器人列表 -->
+              <div
+                class="robotCard"
+                :class="{
+                  checkedCard: item.isChosed,
+                  uncheckedCard: !item.isChosed,
+                  top: item.isMasterControl,
+                }"
+                v-for="(item, index) in list"
+                :key="index"
+                @click="choseCard(item)"
+              >
+                <div class="cardTitle common-font flex-between">
+                  <div v-if="item.connectMode == 'wifi'" class="flex-center">
+                    <img
+                      style="
+                        width: 1.5417vw;
+                        height: 1.125vw;
+                        margin-right: 0.375vw;
+                      "
+                      src="@/assets/images/icon_Wi-Fi.png"
+                    />
+                    <span>{{ $t("wifiConnected") }}</span>
+                  </div>
+                  <div v-else-if="item.connectMode == 'ap'" class="flex-center">
+                    <img
+                      style="
+                        width: 1.3542vw;
+                        height: 1.25vw;
+                        margin-right: 0.375vw;
+                      "
+                      src="@/assets/images/icon_ap.png"
+                    />
+                    <span>{{ $t("APconnection") }}</span>
+                  </div>
+                  <div v-else class="flex-center">
+                    <div class="redPoint"></div>
+                    <span style="color: #ff6656">{{ $t("notConnected") }}</span>
+                  </div>
+                  <span
+                    v-if="item.isMasterControl && item.connectMode != 'none'"
+                    >{{ $t("masterControl") }}</span
+                  >
+                  <img
+                    v-else-if="
+                      !item.isMasterControl && item.connectMode != 'none'
+                    "
+                    style="width: 1vw; height: 1.125vw"
+                    src="@/assets/images/btn_topping.png"
+                    @click.stop="toTop(item)"
+                  />
+                </div>
+                <div class="cardDivider"></div>
+                <div class="cardContent flex-center">
+                  <img
+                    style="width: 6.1849vw; height: 12.5vw"
+                    src="@/assets/images/image_robotModel.png"
+                  />
+                  <span style="font-size: 1.6667vw; margin-top: 1.25vw"
+                    >GR-0001</span
+                  >
+                  <span style="opacity: 0.5; margin-top: 0.625vw"
+                    >GR-1 Lite</span
+                  >
+                </div>
+              </div>
+            </div>
+          </el-carousel-item>
+        </el-carousel>
       </div>
+      <!-- 如果有更多的机器人。。。 -->
       <div v-else class="robotBox">
         <el-carousel
           indicator-position="none"
@@ -21,7 +106,7 @@
           <el-carousel-item v-for="(list, i) in carouselList" :key="i">
             <!-- 添加机器人 -->
             <div class="carouselItem">
-              <div class="addCard" v-if="i == 0">
+              <div class="addCardMore" v-if="i == 0">
                 <div class="add">
                   <img class="addImg" src="@/assets/images/icon_add.png" />
                 </div>
@@ -29,7 +114,7 @@
               </div>
               <!-- 机器人列表 -->
               <div
-                class="robotCard"
+                class="robotCardMore"
                 :class="{
                   checkedCard: item.isChosed,
                   uncheckedCard: !item.isChosed,
@@ -100,6 +185,11 @@
             </div>
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <div class="cardPointSr flex-between">
+        <div class="pointSr" :class="{ pointSrActivity: actIndex == 2 }"></div>
+        <div class="pointSr" :class="{ pointSrActivity: true }"></div>
+        <div class="pointSr" :class="{ pointSrActivity: actIndex == 1 }"></div>
       </div>
       <div class="completeBtn flex-center" @click="completeChose()">
         {{ $t("completeSelection") }}
@@ -188,7 +278,7 @@ export default {
         },
       ],
       carouselList: [],
-      moreRobot: false
+      moreRobot: true,
     };
   },
   created() {},
@@ -238,6 +328,30 @@ export default {
   }
 }
 .addCard {
+  width: 21.7708vw;
+  height: 26.9792vw;
+  border-radius: 0.625vw;
+  border: 0.0521vw dashed #0075b8;
+  order: -2;
+  flex-direction: column;
+  .add {
+    width: 10.4167vw;
+    height: 10.4167vw;
+    background: rgba(68, 216, 251, 0.1);
+    border-radius: 50%;
+    .addImg {
+      width: 3.125vw;
+      height: 3.125vw;
+    }
+  }
+  .addContent {
+    margin-top: 3.6979vw;
+    font-family: AlibabaPuHuiTiM;
+    font-size: 1.6667vw;
+    color: #ffffff;
+  }
+}
+.addCardMore {
   width: 21.5417vw;
   height: 12.3333vw;
   border-radius: 0.625vw;
@@ -279,9 +393,32 @@ export default {
   .carouselItem {
     display: flex;
     flex-wrap: wrap;
-    gap: 2.4583vw;
+    gap: 2.0833vw;
   }
   .robotCard {
+    width: 21.8229vw;
+    height: 27.0313vw;
+    border-radius: 0.8333vw;
+    color: #ffffff;
+    font-size: 1vw;
+    .cardTitle {
+      padding-left: 1.5625vw;
+      padding-right: 1.4063vw;
+      height: 3.0208vw;
+      align-items: center;
+    }
+    .cardDivider {
+      width: 21.875vw;
+      height: 0.0521vw;
+      background: #44d8fb;
+      opacity: 0.3;
+    }
+    .cardContent {
+      height: 24.0104vw;
+      flex-direction: column;
+    }
+  }
+  .robotCardMore {
     width: 21.5417vw;
     height: 12.25vw;
     border-radius: 0.625vw;
@@ -352,7 +489,7 @@ export default {
   //   -50%,
   //   0
   // );
-  margin-top: 8.6458vw;
+  margin-top: 4.6875vw;
   width: 18.4583vw;
   height: 4.1667vw;
   background: linear-gradient(230deg, #198bff 0%, #0086d1 100%);
@@ -364,7 +501,7 @@ export default {
 }
 @media screen and (min-width: 2400px) {
   .completeBtn {
-    margin-top: 5.2083vw;
+    margin-top: 3.125vw;
   }
 }
 .redPoint {
@@ -425,6 +562,27 @@ export default {
     .right {
       background-image: url("../../assets/images/image_apmode.png");
     }
+  }
+}
+.cardPointSr {
+  width: 5.1667vw;
+  height: 0.75vw;
+  margin-top: 3.125vw;
+  .pointSr {
+    width: 0.75vw;
+    height: 0.75vw;
+    background: #44d8fb;
+    border-radius: 0.5vw;
+    opacity: 0.2;
+  }
+  .pointSrActivity{
+    opacity: 1;
+    width: 2.4583vw;
+  }
+}
+@media screen and (min-width: 2400px) {
+  .cardPointSr {
+    margin-top: 2.0833vw;
   }
 }
 </style>
