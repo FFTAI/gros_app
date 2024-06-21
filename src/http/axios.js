aa5cb6657de723abccc2468f4ba5c42c57f8808f
万叶 <41570069+Jy03684315@users.noreply.github.com>
2024-06-21 11:43:44 +0800

    Merge branch '#Tier2' into #tier2_log

--- src/http/axios.js
+++ src/http/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_URL.slice(0,-4),
+  baseURL: process.env.VUE_APP_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
